c00e519e9bea149a6524587a32eadedf4522bc64
Tim <160471079+Simtim1@users.noreply.github.com>
2024-05-22 23:30:49 +1000

    Update create_insert.sql

--- create_insert.sql
+++ create_insert.sql
@@ -34,7 +34,7 @@ INSERT INTO Sales VALUES ('AirB747-400Dra', '1/400', 'AirPacificB747-400', 49.99
 INSERT INTO Sales VALUES ('EmiB777-31HDra', '1/400', 'EmiratesB777-31H', 30, 0, 30, NULL, 30, 1, '2024/2/10');
 INSERT INTO Sales VALUES ('EmiA380-800Dra', '1/400', 'EmiratesA380-800', 59.99, 14.99, 74.98, -10.34, 64.64, 1, '2024/2/12');
 INSERT INTO Sales VALUES ('Fed777-200FDra', '1/400', 'FedExPandaB777-200F', 50, 0, 50, -7, 43, 1, '2024/4/3');
-INSERT INTO Sales VALUES ('B-1stLancerDra', '1/400', 'B-1BLancerLastLancer', 30, 14.99, 44.99, -6.33, 38.66, 1, '2024/4/4');
+INSERT INTO Sales VALUES ('B-1stLancerDra1', '1/400', 'B-1BLancerLastLancer', 30, 14.99, 44.99, -6.33, 38.66, 1, '2024/4/4');
 INSERT INTO Sales VALUES ('AmeConcordeDra', '1/400', 'AmericanWorldAirwaysConcorde', 60, 10, 70, -9.68, 60., 1, '2024/4/12');
 INSERT INTO Sales VALUES ('AirA380-861Apo', '1/400', 'AirFrance80thAnniversaryA380-861', 60, 10, 70, -9.68, 60.32, 6, '2024/4/17');
 INSERT INTO Sales VALUES ('Ames707-320Gem', '1/400', 'AmericanAirlines707-320', 36, 0, 36, -7.04, 28.96, 2, '2024/4/17');
@@ -45,14 +45,14 @@ INSERT INTO Sales VALUES ('VirB737-800Gem', '1/400', 'VirginBlueB737-800', 80, 1
 INSERT INTO Sales VALUES ('AirA340-211Dra', '1/400', 'AirTahitiNuiA340-211', 35, 14.5, 49.5, -6.93, 42.57, 1, '2024/4/21');
 INSERT INTO Sales VALUES ('Nor-10-30ERDra', '1/400', 'NorthwestAirlinesDC-10-30ER', 60, 0, 60, -8.34, 51.66, 1, '2024/4/21');
 INSERT INTO Sales VALUES ('Pol-124-100Gem', '1/400', 'PoletAviakompaniaAN-124-100', 100, 14.5, 114.5, -15.64, 98.86, 2, '2024/4/22');
-INSERT INTO Sales VALUES ('B-1stLancerDra', '1/400', 'B-1BLancerLastLancer', 30, 14.5, 44.5, NULL, 44.5, 1, '2024/4/23');
+INSERT INTO Sales VALUES ('B-1stLancerDra2', '1/400', 'B-1BLancerLastLancer', 30, 14.5, 44.5, NULL, 44.5, 1, '2024/4/23');
 INSERT INTO Sales VALUES ('Uni3ASentryDra', '1/400', 'UnitedStatesAirForceE-3ASentry', 30, NULL, 30, NULL, 30, 1, '2024/4/23');
 INSERT INTO Sales VALUES ('Figry"F-16CDra', '1/72', 'FightingFalcon"FighterWeaponsSchool50thAnniversary"F-16C', 44, 14.5, 58.5, -8.14, 50.36, 1, '2024/4/25');
 INSERT INTO Sales VALUES ('AtlsIL-76TDBig', '1/500', 'Atlant-SoyuzAirlinesIL-76TD', 18, 0, 18, -16.12, 1.88, 5, '2024/4/25');
 INSERT INTO Sales VALUES ('AmealMetal)Sch', '1/500', 'AmericanAirlinesB777-200(RealMetal)', 18, 14.5, 32.5, NULL, 32.5, 3, '2024/4/25');
 INSERT INTO Sales VALUES ('QanrmulaOneSch', '1/500', 'QantasBoeing747-400FormulaOne', 35, 14.5, 49.5, NULL, 49.5, 3, '2024/4/25');
 INSERT INTO Sales VALUES ('VirSunsA321Sch', '1/500', 'VirginSunsA321', 18, 0, 18, NULL, 18, 3, '2024/4/25');
-INSERT INTO Sales VALUES ('UniWingAMC"Dra', '1/400', 'UnitedStatesAirForceC-5BGalaxy"436thAirliftWingAMC"', 70, 14.5, 84.5, -18.33, 66.17, 1, '2024/4/26',);
+INSERT INTO Sales VALUES ('UniWingAMC"Dra', '1/400', 'UnitedStatesAirForceC-5BGalaxy"436thAirliftWingAMC"', 70, 14.5, 84.5, -18.33, 66.17, 1, '2024/4/26');
 INSERT INTO Sales VALUES ('UniorceE-4BDra', '1/400', 'UnitedStatesAirForceE-4B', 50, 0, 50, NULL, 50, 1, '2024/4/26');
 INSERT INTO Sales VALUES ('AirA320-211Dra', '1/400', 'AirCanadaA320-211', 20, 14.5, 34.5, -4.92, 29.58, 1, '2024/4/27');
 INSERT INTO Sales VALUES ('Emidlife#3"Gem', '1/400', 'EmiratesA380-861"Wildlife#3"', 70, 14.5, 84.5, -11.62, 72.88, 2, '2024/4/29');
